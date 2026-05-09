25704456646dac0b22555b7413d4dd0c45dec3f4
Siyam Hasan <ratkim158@gmail.com>
2026-05-08 19:12:20 -0700

    Add files via upload

---
+++ Print a 10x10 grid of dashes.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+     //Print a 10x10 grid of dashes
+        for(int i = 1;i<=10;i++){
+            for(int j = 1;j<=10;j++){
+                System.out.print(" - ");
+            }
+            System.out.println();
+        }
+    }
+}
